eb52eca515355ae127aa924d39d5a89c45a8dc39
Mackinlay Kevin <kemack83@gmail.com>
2026-09-02 16:12:56 -0300

    nuevo componente lista-usuarios y configuracion de la ruta /usuarios.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {ListaUsuarios} from "./components/lista-usuarios/lista-usuarios";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'usuarios',
+        pathMatch: 'full'
+    },
+    {
+        path: 'usuarios',
+        component: ListaUsuarios
+    }
+];
