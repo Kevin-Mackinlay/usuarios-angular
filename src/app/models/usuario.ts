0dd1d3807af41287b7b1cbceefbc3bcb01e35677
Mackinlay Kevin <kemack83@gmail.com>
2026-09-02 17:13:55 -0300

    modificacion en app.config.ts, creacion del modelo usuario, y tambien se creo el service usuario.

---
+++ src/app/models/usuario.ts
@@ -0,0 +1,9 @@
+export interface Usuario {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+    website: string;
+}
+
---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,17 @@
+import { inject,Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+  getUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.apiUrl);
+  }
+
+}
