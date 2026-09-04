2036f9440d15f27d7c62c5f5c04ec53072d67958
Mackinlay Kevin <kemack83@gmail.com>
2026-09-03 21:03:26 -0300

    conectando el componente ListaUsuarios con UsuarioService

--- src/app/components/lista-usuarios/lista-usuarios.ts
+++ src/app/components/lista-usuarios/lista-usuarios.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-lista-usuarios',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './lista-usuarios.html',
   styleUrl: './lista-usuarios.css',
 })
-export class ListaUsuarios {}
+export class ListaUsuarios {
+  private readonly usuarioService = inject(UsuarioService);
+
+  readonly usuarios$ = this.usuarioService.getUsuarios();
+}
