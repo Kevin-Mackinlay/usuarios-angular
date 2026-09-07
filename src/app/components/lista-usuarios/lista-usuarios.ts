bc8a5ecba95becb3c22e2c0328109c82ca072d25
Mackinlay Kevin <kemack83@gmail.com>
2026-09-06 22:48:07 -0300

    agregue navegacion al detalle de usuarios

--- src/app/components/lista-usuarios/lista-usuarios.ts
+++ src/app/components/lista-usuarios/lista-usuarios.ts
@@ -1,11 +1,12 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-lista-usuarios',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './lista-usuarios.html',
   styleUrl: './lista-usuarios.css',
 })
