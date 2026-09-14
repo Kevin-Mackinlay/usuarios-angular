9988498a54c4a8c21a9fbda2e47a60c0391b1add
Mackinlay Kevin <kemack83@gmail.com>
2026-09-14 16:13:54 -0300

    Conecto el formulario con la creacion de usuarios

--- src/app/components/formulario-usuario/formulario-usuario.ts
+++ src/app/components/formulario-usuario/formulario-usuario.ts
@@ -1,36 +1,53 @@
-import { Component,inject } from '@angular/core';
-import {FormBuilder,ReactiveFormsModule,Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-formulario-usuario',
-  imports: [ReactiveFormsModule,RouterLink],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './formulario-usuario.html',
   styleUrl: './formulario-usuario.css',
 })
 export class FormularioUsuario {
   private readonly formBuilder = inject(FormBuilder);
+  private readonly usuarioService = inject(UsuarioService);
 
-  readonly formularioUsuario = this.formBuilder.group({
-    nombre: ['', [Validators.required, Validators.minLength(3) ]],
+  //aca tambien tengo dudas
+  readonly formularioUsuario = this.formBuilder.nonNullable.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
     email: ['', [Validators.required, Validators.email]],
-    telefono: ['']
+    telefono: [''],
   });
 
-  formularioEnviado = false;
+  enviando = false;
+  mensajeExito = '';
+  mensajeError = '';
 
   enviarFormulario(): void {
-
-     this.formularioEnviado = false;
-
+    this.mensajeExito = '';
+    this.mensajeError = '';
     if (this.formularioUsuario.invalid) {
-     this.formularioUsuario.markAllAsTouched();
-     return;
+      this.formularioUsuario.markAllAsTouched();
+      return;
     }
+    this.enviando = true;
 
-    console.log('Usuario:',this.formularioUsuario.value);
+    const datos = this.formularioUsuario.getRawValue();
 
-    this.formularioEnviado = true;
-    this.formularioUsuario.reset();
+    this.usuarioService.crearUsuario(datos).subscribe({
+      next: (usuarioCreado) => {
+        this.mensajeExito = `El usuario ${usuarioCreado.name} fue creado correctamente`;
+
+        this.formularioUsuario.reset();
+        this.enviando = false;
+      },
+      error: (error) => {
+        this.mensajeError = 'No se pudo crear el usuario. Intente de nuevo.';
+
+        this.enviando = false;
+      },
+    });
   }
 }
