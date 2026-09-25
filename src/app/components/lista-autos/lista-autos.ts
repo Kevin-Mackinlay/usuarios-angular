c2fb927b42a644b69dd3d10cc30b1107e4170c5c
Mackinlay Kevin <kemack83@gmail.com>
2026-09-24 23:16:16 -0300

    implemente listado de autos con estados

--- src/app/components/lista-autos/lista-autos.ts
+++ src/app/components/lista-autos/lista-autos.ts
@@ -1,9 +1,53 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+
+import { Component, inject } from '@angular/core';
+
+import { RouterLink } from '@angular/router';
+
+import { catchError, map, Observable, of, startWith, Subject, switchMap } from 'rxjs';
+
+import { Auto } from '../../models/auto';
+
+import { AutoService } from '../../services/auto.service';
+
+type EstadoListaAutos =
+  { estado: 'cargando' } | { estado: 'exito'; autos: Auto[] } | { estado: 'error' };
 
 @Component({
   selector: 'app-lista-autos',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './lista-autos.html',
   styleUrl: './lista-autos.css',
 })
-export class ListaAutos {}
+export class ListaAutos {
+  private readonly autoService = inject(AutoService);
+
+  private readonly recargar$ = new Subject<void>();
+
+  readonly estado$: Observable<EstadoListaAutos> = this.recargar$.pipe(
+    startWith(undefined),
+
+    switchMap(() =>
+      this.autoService.getAutos().pipe(
+        map((autos): EstadoListaAutos => ({
+          estado: 'exito',
+          autos,
+        })),
+
+        startWith({
+          estado: 'cargando',
+        } as EstadoListaAutos),
+
+        catchError(() =>
+          of({
+            estado: 'error',
+          } as EstadoListaAutos),
+        ),
+      ),
+    ),
+  );
+
+  reintentar(): void {
+    this.recargar$.next();
+  }
+}
